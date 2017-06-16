05888bdbf5a0d85987a547abae49dde8976f9184
Sam Zeidman <sam.zeidman@gmail.com>
2017-06-16 18:26:51 +0000

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,7 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+    end
+  end
 end
